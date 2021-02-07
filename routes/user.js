94e8c40a7c30c693b73f60d4b23335139fa08c65
hysteria9 <anthonyrov@gmail.com>
2021-02-07 17:55:41 +0100

    update: add isAuthenticated middleware for route /user/:token

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const uid2 = require("uid2");
 const SHA256 = require("crypto-js/sha256");
 const encBase64 = require("crypto-js/enc-base64");
+const isAuthenticated = require("../middlewares/isAuthenticated");
 
 const router = express.Router();
 
