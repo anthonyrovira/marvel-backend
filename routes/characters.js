795f11457d34c39e561ee237dba732dc26e9122b
hysteria9 <anthonyrov@gmail.com>
2021-03-07 21:38:59 +0100

    add: route characters

---
+++ routes/characters.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const router = express.Router();
+const axios = require("axios");
+const qs = require("qs");
+
+router.get("/characters", async (req, res) => {
+  try {
+    let params = {};
+    if (req.query.name) {
+      params = {
+        apiKey: process.env.API_KEY,
+        name: debouncedSearch,
+      };
+    } else if (req.query.limit || req.query.skip) {
+      params = {
+        apiKey: process.env.API_KEY,
+        limit: limit,
+        skip: skip,
+      };
+    }
+
+    const queryParams = qs.stringify(params);
+
+    await axios
+      .get(
+        `${process.env.REACT_APP_REACTEUR_BACKEND_URL}/characters?${queryParams}`
+      )
+      .then((response) => {
+        res.status(200).json(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    res.status(400).json({ error: { message: error.message } });
+  }
+});
+
+module.exports = router;
