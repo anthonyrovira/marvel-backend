28b8ce946835f7eb4ae0b2d7a5c0d3c782c93e1c
hysteria9 <anthonyrov@gmail.com>
2021-03-07 22:09:41 +0100

    add: route comics/:id

--- routes/comics.js
+++ routes/comics.js
@@ -35,4 +35,24 @@ router.get("/comics", async (req, res) => {
   }
 });
 
+router.get("/comics/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const apiKey = process.env.API_KEY;
+
+    await axios
+      .get(
+        `${process.env.URL_BACKEND_LEREACTEUR}/comics/${id}?apiKey=${apiKey}`
+      )
+      .then((response) => {
+        res.status(200).json(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  } catch (error) {
+    res.status(400).json({ error: { message: error.message } });
+  }
+});
+
 module.exports = router;
